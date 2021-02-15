db6c68fd8db348d91903e6d351721cedc6b4863f
Jothamsl <jothamsl1@gmail.com>
2021-02-15 21:38:29 +0100

    Finished Day 1 part 2

--- 1.py
+++ 1.py
@@ -5,18 +5,36 @@ def get_data():
     return [int(s.rstrip('\n')) for s in vals]
 
 
-def main(vals):
-    """
-    ------- DAY 1: Report Repair -------
-    Find the two entries that sum to 2020 then,
-    multiply the two values to get the answer.
-    """
+def part_one(vals):
     for i in vals:
         for j in vals:
             if i + j == 2020:
                 res = i*j
     print(res)
 
+def part_two(vals):
+    for i in vals:
+        for j in vals:
+            for k in vals:
+                if i + j + k == 2020:
+                    res = i * j * k
+    print(res)
+
+
+def main(vals):
+    """
+    ------- DAY 1: Report Repair -------
+
+    --Part one--
+    Find the two entries that sum to 2020 then,
+    multiply the two values to get the result.
+
+    --Part two--
+    Find the three entries that sum to 2020 then,
+    multiply the three values to get the result
+    """
+    part_one(vals)
+    part_two(vals)
 
 if __name__ == "__main__":
     main(get_data())
